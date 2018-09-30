1dd7b8d2d8762a3483317cde09950dc19f0cc2ca
Cleiton Jardim <cleitonjardim@iMac-de-Cleiton.local>
2018-09-30 02:39:10 -0300

    Add support android oreo

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -1,13 +1,18 @@
 package com.gae.scaffolder.plugin;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.app.Notification;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -20,6 +25,45 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FCMPlugin";
+
+    private String getStringResource(String name) {
+        return this.getString(this.getResources().getIdentifier(name, "string", this.getPackageName()));
+    }
+
+    public class NotificationUtils extends ContextWrapper {
+
+        private NotificationManager mManager;
+        public static final String ANDROID_CHANNEL_ID = "br.davinti.Vitruvio.Vitruvio";
+        public static final String ANDROID_CHANNEL_NAME = "Atividades";
+
+        public NotificationUtils(Context base) {
+            super(base);
+            createChannels();
+        }
+
+        public void createChannels() {
+            // create android channel
+            NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID,
+            ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            // Sets whether notifications posted to this channel should display notification lights
+            androidChannel.enableLights(true);
+            // Sets whether notification posted to this channel should vibrate.
+            androidChannel.enableVibration(true);
+            // Sets the notification light color for notifications posted to this channel
+            androidChannel.setLightColor(Color.GREEN);
+            // Sets whether notifications posted to this channel appear on the lockscreen or not
+            androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            getManager().createNotificationChannel(androidChannel);
+        }
+
+        private NotificationManager getManager() {
+            if (mManager == null) {
+                mManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            return mManager;
+        }
+    }
 
     /**
      * Called when message is received.
@@ -68,6 +112,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        String channelId = this.getStringResource("default_notification_channel_id");
+        String channelName = this.getStringResource("default_notification_channel_name");
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(getApplicationInfo().icon)
@@ -79,6 +125,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
